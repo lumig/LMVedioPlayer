95ba5e779a85f06e8086f13bb29528904116c729
Ming Lu <583383489@qq.com>
2015-08-31 14:01:35 +0800

    Initial Commit

---
+++ LMVedioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LMVedioPlayer
+//
+//  Created by mac on 15/8/31.
+//  Copyright (c) 2015年 migi Lu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
